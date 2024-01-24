28c67906fef00e32ca1d79e73b9f4ab812909d1c
mathV-ui <128636626+mathV-ui@users.noreply.github.com>
2024-01-24 21:34:22 +0100

    Logo but-info

--- Views/view_begin.php
+++ Views/view_begin.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8"/>
+    	<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title> But-Info</title>
-		<!-- <link rel="stylesheet" href="Content/css/style.css"/> -->
+		<!-- Importer l'icône du site -->
+		<link rel="icon" href="Content/img/but-info_logo.ico" type="image/x-icon">
+
+		<!-- Importer la feuille de style CSS -->
+		<link rel="stylesheet" href="Content/css/styles.css">
+
+		<!-- Importer le script JavaScript -->
+		<script src="Content/js/script.js" defer></script>
 	</head>
 	<body>
